13222210993366766c756af196870bbf2dae4ef1
Johny Wills <jonathan_wills@icloud.com>
2021-03-24 01:48:19 +0900

    :sparkles: very small change to PrivateRoute code

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,25 +1,11 @@
-import { useDisclosure } from "@chakra-ui/react";
 import React from "react";
-import { Route, useHistory } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import Projects from "../Projects";
-import SignIn from "./Navbar/SignIn";
 
 const PrivateRoute = ({ component: Component, ...rest }: any) => {
 	// Auth Context
 	const { user } = useAuth();
-
-	// * ChakraUI
-	// Modal hooks
-	const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true });
-
-	// React Router
-	const history = useHistory();
-
-	const custOnClose = () => {
-		onClose();
-		history.push("/projects");
-	};
 
 	return (
 		<Route
